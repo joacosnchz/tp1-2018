85098dea534495fcf1b9432cfd8233eb947308e2
Joaquin Sanchez <sanchezjoaquin1995@gmail.com>
2018-08-26 14:36:24 -0300

    Edicion de lista

--- src/app/carddetail/carddetail.component.ts
+++ src/app/carddetail/carddetail.component.ts
@@ -9,11 +9,14 @@ import { TrelloService }  from '../../trello/trello.api';
 })
 export class CardDetailComponent implements OnInit {
     @Input() card:any = {};
+    lists;
     mostrarError = false;
 
     constructor(private router : Router, private route: ActivatedRoute, private api : TrelloService) { }
 
     ngOnInit() {
+        this.lists = this.api.getLists();
+
         this.route.params.subscribe(params => {
             this.card.id = params['id'];
             this.api.getCard(this.card).subscribe(card => {
@@ -28,6 +31,7 @@ export class CardDetailComponent implements OnInit {
         this.api.update(this.card).subscribe(() => {
             this.router.navigate(['/cards']);
         }, error => {
+            console.log(error);
             this.mostrarError = true;
             e.target.innerHTML = 'Aceptar';
             e.target.className = 'btn btn-primary';
--- src/trello/trello.api.ts
+++ src/trello/trello.api.ts
@@ -6,6 +6,9 @@ export class TrelloService {
     private endpoint = 'https://api.trello.com/1';
     private credentials = 'key=7826a7def1803a362b55c90ac66ab5f1&token=397ec8296006a8e06835afdcbe0b3e4e6bacce8848945ac19870599d171a0959';
     private board = 'CeLcNDVU';
+    private lists = [{id: '5b6b420b6125170fa01f86f0', name: 'To Do'},
+        {id: '5b6b420c4dd9240f4b1e4f3f', name: 'Doing'},
+        {id: '5b6b420d3579f37d55b18226', name: 'Done'}];
 
     constructor(private http: HttpClient) {
         
@@ -28,6 +31,20 @@ export class TrelloService {
     }
 
     update(card) {
-        return this.http.put(this.endpoint + '/cards/' + card.id + '?' + this.credentials, {name: card.name, desc: card.desc});
+        return this.http.put(this.endpoint + '/cards/' + card.id + '?' + this.credentials, {name: card.name, desc: card.desc, idList: card.idList});
+    }
+
+    getLists() {
+        return this.lists;
+    }
+
+    getList(id) {
+        for(let i = 0; i < this.lists.length;i++) {
+            if(this.lists[i].id == id) {
+                return this.lists[i];
+            }
+        }
+
+        return null;
     }
 }
