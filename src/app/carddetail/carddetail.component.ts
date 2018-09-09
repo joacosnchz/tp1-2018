ae3f088af5ce402c28842b444d996742a6a8546a
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-09-08 21:05:40 -0300

    Refactor Details and NewCard into one component

--- src/app/carddetail/carddetail.component.ts
+++ src/app/carddetail/carddetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { TrelloService }  from '../../trello/trello.api';
 
@@ -9,47 +9,111 @@ import { TrelloService }  from '../../trello/trello.api';
 })
 export class CardDetailComponent implements OnInit {
     @Input() card:any = {};
+
     lists;
     mostrarError = false;
     mostrarErrorAdjuntos = false;
-    attachments;
+    mostrarErrorCrear = false;
+    mostrarErrorCrearAdjunto = false;
 
-    constructor(private router : Router, private route: ActivatedRoute, private api : TrelloService) { }
+    constructor(private router : Router, 
+                private route: ActivatedRoute, 
+                private api : TrelloService) { }
 
     ngOnInit() {
         this.lists = this.api.getLists();
 
         this.route.params.subscribe(params => {
-            this.card.id = params['id'];
-            this.api.getCard(this.card).subscribe(card => {
-                this.card = card;
-            });
-
-            this.loadAttachments();
+            if ( params['id'] ){
+                this.loadCardById(params['id']);                
+            } else {
+                this.initCard();
+            }
+            
         });
     }
+
+    loadCardById(id){
+        this.card.id = id;
+        this.api.getCard(this.card).subscribe(card => {
+            this.card = card;
+            this.loadAttachments();
+        });        
+    }
+
+    initCard() {
+		this.card = {
+			id: '',
+			name: '',
+            desc: '',
+            idList: '',
+            attachment: null, // Only for adding an attachment
+		};
+	}
 
     loadAttachments() {
         this.api.getCardAttachments(this.card.id).subscribe(attachments => {
-            this.attachments = attachments;
+            this.card.attachments = attachments;
         });
     }
 
-    onSubmit(e) {
+    onClickAceptar(e) {
         e.preventDefault();
         e.target.innerHTML = 'Cargando...';
         e.target.disabled = true;
         e.target.className = 'btn btn-secondary';
 
+        if (this.card.id !== ''){
+            this.updateCard(e);
+            this.addAttachment(e, this.card);
+        } else {
+            this.createCard(e);
+        }
+    }
+    
+    updateCard(e){
         this.api.update(this.card).subscribe(() => {
             this.router.navigate(['/cards']);
-        }, error => {
-            console.log(error);
+        }, err => {
+            console.log(err);
             this.mostrarError = true;
             e.target.innerHTML = 'Aceptar';
             e.target.className = 'btn btn-primary';
             e.target.disabled = false;
         });
+    }
+
+    createCard(e){
+        this.api.newCard(this.card).subscribe((createdCard:any) => {
+            this.addAttachment(e, createdCard);
+        }, err => { 
+            console.log(err);
+            this.mostrarErrorCrear = true;
+            e.target.innerHTML = 'Aceptar';
+            e.target.className = 'btn btn-primary';
+            e.target.disabled = false;
+        }); 
+    }
+
+    fileChange(e) {
+        this.card.attachment = e.target.files[0];
+    }
+
+    addAttachment(e, card){
+        if (this.card.attachment !== null){ 
+            
+            let previousText = e.target.innerHTML;
+            e.target.innerHTML = 'Cargando...';
+
+            this.api.newAttachment(card.id, this.card.attachment).subscribe(() => {
+                this.router.navigate(['/cards']);
+                e.target.innerHTML = previousText;
+            }, err => { 
+                console.log(err);
+                this.mostrarErrorCrearAdjunto = true;
+                e.target.innerHTML = previousText;
+             });
+        }
     }
 
     deleteAttachment(e, idAttachment) {
